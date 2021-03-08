51082df8d14fc49e84ff354f44210c7a45201bc6
Mouslli <abdullah.mouslli@gmail.com>
2021-03-08 12:44:14 -0700

    images added and working buttons

--- js/scripts.js
+++ js/scripts.js
@@ -27,6 +27,19 @@ function Image (imgUrl, tags) {
 var images = [
 	new Image ('<img src ="pics/Dorothy.jpg">', ["Birds", "Abandonded", "Old","All"]),
 	new Image ('<img src ="pics/Dorothy1.jpg">', ["House", "Old","Sky", "Clouds", "Abandonded","All"]),
+	new Image ('<img src ="pics/Dorothy2.jpg">', ["Swing","Coulees", "Abandonded",  "All"]),
+	new Image ('<img src ="pics/Granum2.jpg">', ["House", "Abandonded", "All", "Grass", "Wood"]),
+	new Image ('<img src ="pics/Granum2.jpg">', ["House", "Abandonded", "All", "Grass", "Wood"]),
+	new Image ('<img src ="pics/Granum3.jpg">', ["House", "Abandonded", "All", "Grass", "Wood"]),
+	new Image ('<img src ="pics/Mtn2.jpg">', ["Landscape", "Mountains", "Clouds","Trees", "All"]),
+	new Image ('<img src ="pics/Mtn3.jpg">', ["Landscape", "Mountains", "Clouds","Sky", "All"]),
+	new Image ('<img src ="pics/Nordegg.jpg">', ["House", "Abandonded","Trees", "All"]),
+	new Image ('<img src ="pics/Arc.jpg">', ["Architecture", "Building","Glass", "Windows", "Sky", "All"]),
+	new Image ('<img src ="pics/Arc1.jpg">', ["Architecture", "Building","Glass", "Windows", "All"]),
+	new Image ('<img src ="pics/Arc2.jpg">', ["Architecture", "Building","Glass", "Windows", "Clouds", "Sky", "All"]),
+	new Image ('<img src ="pics/Chrch.jpg">', ["Architecture", "Building", "Sky", "All"]),
+	new Image ('<img src ="pics/Chrch1.jpg">', ["Architecture", "Sky", "All"]),
+	new Image ('<img src ="pics/Chrch2.jpg">', ["Architecture", "Building", "Glass", "Windows", "All"])
 
 ]
 
@@ -42,3 +55,16 @@ images.forEach(function(image){
 
 })
 
+
+$(".filter").on("click", function () {
+
+	var tag = $(this).text();
+	console.log(tag);
+	$("." + tag).fadeIn();
+	$(".image").not("."+tag).hide();
+
+	$(".filter").removeClass("active");
+	$(this).addClass("active");
+
+
+})
